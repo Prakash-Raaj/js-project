d91e51d83acbc68359659de23cd22c5bffb9d401
Harikrishnan-HK <krishnanhari@outlook.com>
2023-08-12 16:30:36 -0400

    edit and delete functionality added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const User = mongoose.model('User', {
 });
 
 const Page = mongoose.model('Page', {
-  title: String,
+  pagetitle: String,
   imgPath: String,
   description: String,
 });
@@ -150,7 +150,7 @@ myApp.post('/addpage', (req, res) => {
     }
   });
   var addData = {
-    title: req.body.pagetitle,
+    pagetitle: req.body.pagetitle,
     imgPath: '/Images/' + imageFileName,
     description: req.body.description,
   };
@@ -164,8 +164,8 @@ myApp.post('/addpage', (req, res) => {
 myApp.get('/editPages', (req, res) => {
   Page.find({})
     .then((pages) => {
-      console.log('Pages length', pages.length);
-      console.log('The pages are', pages);
+      // console.log('Pages length', pages.length);
+      // console.log('The pages are', pages);
       res.render('editPages', { pages: pages });
     })
     .catch((err) => {
@@ -174,9 +174,64 @@ myApp.get('/editPages', (req, res) => {
 });
 
 myApp.get('/editpage/:pageId', (req, res) => {
-  res.render('editpage');
+  var pageId = req.params.pageId;
+  Page.findOne({_id:pageId})
+    .then((page) => {
+      // console.log('Edit page',page)
+      res.render('editpage',page);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
 });
 
+myApp.get('/delete/:pageId', (req,res) => {
+  var pageId = req.params.pageId;
+  Page.findOneAndDelete({_id:pageId})
+  .then(function() {
+    res.render('delete');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+})
+
+myApp.post('/editpage/:pageId',(req,res)=>{
+  Page.findOne({_id:req.params.pageId})
+    .then((page) => {
+        console.log('check for page page',page)
+        var imagePath = page.imgPath;
+      if(req.files !== null){
+        var imageFile = req.files.pageImg;
+        var imageFileName = imageFile.name;
+        imagePath = folderPath + imageFileName;
+        imageFile.mv(imagePath, function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+    }
+  
+  var pageData = {
+    pagetitle:req.body.pagetitle,
+    imgPath:imagePath,
+    description:req.body.description
+  }
+  Page.findByIdAndUpdate(req.params.pageId,pageData).
+  then((page)=>{
+    console.log('Page updated',page);
+    res.render('edited');
+  }).catch(err=>{
+    console.log(err);
+  })
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  }
+)
 
 
 myApp.listen('8080');
